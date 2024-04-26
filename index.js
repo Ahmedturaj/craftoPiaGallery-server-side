abef593ca0459086fef2a51727a906c701948c5b
Ahmedturaj <ahmedturaj12@gmail.com>
2024-04-26 15:19:31 +0600

    added post method for arts

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(cors({
     origin: ["http://localhost:5173"],
     credentials: true,
-  })
+})
 )
 app.use(express.json());
 
@@ -19,26 +19,31 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    const userCollections = client.db("craftopia").collection("craftopia");
-    
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        const infoCollections = client.db("craftopia").collection("craftopia");
+        app.post('/arts', async (req, res) => {
+            const info = req.body;
+            const result = await infoCollections.insertOne(info)
+            res.send(result)
+            console.log(result);
+        })
 
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
